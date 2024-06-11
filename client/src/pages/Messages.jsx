29141db4b7963a8c63293dcc677187370f11a425
felabs1 <felixawere01@gmail.com>
2024-06-12 02:12:27 +0300

    chat container design done

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -4,6 +4,7 @@ import SideNav from "../components/navigation/SideNav";
 import Main from "../components/middlepage/Main";
 import MobileSidenav from "../components/navigation/MobileSidenav";
 import ContactNavigation from "../components/navigation/ContactNavigation";
+import ChatBox from "../components/middlepage/ChatBox";
 
 const Messages = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -24,7 +25,9 @@ const Messages = () => {
           <div className={`w3-col l3`}>
             <ContactNavigation />
           </div>
-          <div className={`w3-col l6`}>chat box</div>
+          <div className={`w3-col l6`} style={{ height: "100%" }}>
+            <ChatBox />
+          </div>
           <div className={`w3-col l3`}>chat details</div>
         </div>
       </Main>
