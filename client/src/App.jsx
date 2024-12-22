2ea11319d8620f5d9cfe7b9cc214a053b7269ddd
felabs1 <felixawere01@gmail.com>
2024-12-22 22:27:19 +0300

    community page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import AppProvider from "./providers/AppProvider";
 import NewCommunity from "./pages/community_essentials/NewCommunity";
 import NewVideo from "./components/middlepage/video_essentials/NewVideo";
 import UserProfile from "./pages/profile_essentials/UserProfile";
+import Communities from "./pages/Communities";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,6 +30,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/communities/:id" element={<Communities />} />
             <Route path="/explore" element={<Explore />} />
             <Route path="/messages" element={<Messages />} />
             <Route path="/more" element={<More />} />
